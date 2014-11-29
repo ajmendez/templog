3f75b6c051f2849ebb5f1b6966ea7e31e18fac8e
Alexander Mendez <bluespace@gmail.com>
2014-11-29 00:17:34 -0500

    added new plot

--- bin/plot_temperature.py
+++ bin/plot_temperature.py
@@ -5,8 +5,9 @@ import sys
 import pylab
 import numpy as np
 from dateutil import parser
-from pysurvey.plot import setup, dateticks
-
+from pysurvey.plot import setup, dateticks, minmax
+from matplotlib.dates import date2num
+import matplotlib.ticker
 
 FILENAME = os.path.expanduser('~/.temperature.neon.log')
 
@@ -20,18 +21,54 @@ def read_temps(filename=FILENAME):
             out.append(dict(date=parser.parse(items[1]),
                             temperature=float(items[2])/1000.0))
     return out
+
+
+def setupplot(secondax=False, **kwargs):
+    ytickv = np.linspace(30,50,6)
+    yticknames = map('{:0.0f}'.format, ytickv)
+    tmp = dict(
+        ylabel='Temperature [c]',
+        yr=minmax(ytickv), ytickv=ytickv,
+        yticknames=yticknames,
+    )
+    tmp.update(kwargs)
+    ax = setup(**tmp)
     
+    if secondax:
+        subplt = kwargs.get('subplt',None)
+        f = lambda x: '{:0.0f}'.format(1.8*x + 32.0)
+        yticknames = map(f, ytickv)
+        ax2 = ax.twinx()
+        ax2.set_ylabel(r"Temperature [F]")
+        ax2.set_ylim(minmax(ytickv))
+        ax2.yaxis.set_major_locator(matplotlib.ticker.FixedLocator(ytickv))
+        ax2.yaxis.set_major_formatter(matplotlib.ticker.FixedFormatter(yticknames))
+        pylab.sca(ax)
+        
+        # setup(ax=ax.twinx(),
+        #       subplt=subplt,
+        #       ylabel='Temperature [F]',
+        #       yr=minmax(ytickv), ytickv=ytickv, yticknames=yticknames)
+        
+    
+    return ax
+
 
 def plot_temp():
     data = read_temps()
     dates, values = zip(*[(d['date'], d['temperature'])
                           for d in data])
-    # setup(figsize=(12,12))
+    setup(figsize=(12,6))
     
-    setup(#subplt=(2,2,1), 
-          xlabel='Date', ylabel='Temp [c]')
+    setupplot(subplt=(1,2,1), autoticks=True, xlabel='Date',)
     pylab.plot(dates, values)
-    dateticks('%Y.%m.%h')
+    dateticks('%Y.%m.%d')
+    
+    tmp = (date2num(dates) % 1.0)*24.0
+    setupplot(subplt=(1,2,2), autoticks=True, xlabel='Hour of Day')
+    pylab.plot(tmp, values, '.')
+    setupplot(subplt=(1,2,2), ylabel='', secondax=True)
+    
     
     pylab.show()
 
