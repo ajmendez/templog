8e46ffbb00f2a89a4ed52ee0597215220cc21c9a
Alexander Mendez <bluespace@gmail.com>
2015-01-18 21:46:24 -0500

    working on xenon

--- bin/get_temperature.sh
+++ bin/get_temperature.sh
@@ -3,4 +3,7 @@
 
 rsync -ravpP pi:.temperature.neon.log  ~/.temperature.neon.log
 
+
+# rsync -ravpP aether:.temperature.xenon.log  ~/.temperature.xenon.log
+
 # rsync -ravpP xenon:.temperature.xenon.log  ~/
