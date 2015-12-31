873438acfeb4c28c5afe51d6e6bb7bfa5fed37fd
Alexander Mendez <bluespace@gmail.com>
2015-12-30 22:30:02 -0500

    added osx support

--- bin/run_temperature.sh
+++ bin/run_temperature.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # gets some temperature data.
 
-echo "$(date +%s) | $(date) | $(cat /sys/class/thermal/thermal_zone0/temp) | $(/opt/vc/bin/vcgencmd measure_temp)"
+if [ -e "/usr/local/bin/osx-cpu-temp" ]; then
+    echo "$(date +%s) | $(date) | $(/usr/local/bin/osx-cpu-temp)"
+else
+    echo "$(date +%s) | $(date) | $(cat /sys/class/thermal/thermal_zone0/temp) | $(/opt/vc/bin/vcgencmd measure_temp)"
+fi
